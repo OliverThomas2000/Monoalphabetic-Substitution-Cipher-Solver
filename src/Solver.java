9aa58777e2288b163424b204609f98a9f105026e
Oliver Thomas <ojthomas1@sheffield.ac.uk>
2020-06-05 17:51:28 +0100

    stored our value of 'e'

--- src/Solver.java
+++ src/Solver.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 public class Solver {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String cipherText = "the quick brown fox jumped over the lazy dog";
+        CipherMap currentDecrypted = new CipherMap();
         LetterFrequencyCounter letterFrequency = new LetterFrequencyCounter();
         letterFrequency.letterCounter(cipherText.strip().toUpperCase());
-        System.out.println(letterFrequency.charMap);
-        LinkedHashMap<Character,Integer> orderedFrequencies =  MapSortByValue.MapSortDescending(letterFrequency.charMap);
+
+        LinkedHashMap<Character, Integer> orderedFrequencies =
+                MapSortByValue.MapSortDescending(letterFrequency.charMap);
         Character[] mostCommon = orderedFrequencies.keySet().toArray(new Character[0]);
-        System.out.println(mostCommon[0]);
-        //TODO store this 'e' in our cipher to plaintext map!
+
+        currentDecrypted.addPair(mostCommon[0], 'E');
         //TODO create a class that find three letter words ending in our 'e'
     }
 }
