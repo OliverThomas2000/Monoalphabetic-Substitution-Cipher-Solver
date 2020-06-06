2970aceab3626256822fbd6cb8100907ce47beb4
Oliver Thomas <ojthomas1@sheffield.ac.uk>
2020-06-06 20:55:20 +0100

    Added a function to word filter to be able to return words that end with a particular letter.

--- src/Solver.java
+++ src/Solver.java
@@ -3,11 +3,7 @@ import java.util.*;
 public class Solver {
     public static void main(String[] args) {
         String cipherText =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et" +
-                " dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-                "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
-                "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
-                "officia deserunt mollit anim id est laborum.";
+                "the quick brown fox jumps over the lazy dog";
         CipherMap currentDecrypted = new CipherMap();
         WordTypeFilter filter = new WordTypeFilter(cipherText.toUpperCase());
         LetterFrequencyCounter letterFrequency = new LetterFrequencyCounter();
@@ -17,7 +13,7 @@ public class Solver {
                 MapSortByValue.MapSortDescending(letterFrequency.charMap);
         Character[] mostCommon = orderedFrequencies.keySet().toArray(new Character[0]);
         currentDecrypted.addPair('E', mostCommon[0]);
-        System.out.println(filter.returnXLetterWords(3)[0]);
-        //TODO extend filter class to return words ending in a particular letter.
+        String[] threeLetterWords = filter.returnXLetterWords(3);
+        System.out.println(filter.returnEndsWithX('E')[0]);
     }
 }
--- src/WordTypeFilter.java
+++ src/WordTypeFilter.java
@@ -16,4 +16,14 @@ public class WordTypeFilter {
         String[] output = outputList.toArray(new String[outputList.size()]);
         return output;
     }
+    public String[] returnEndsWithX(char c){
+        ArrayList<String> outputList = new ArrayList<String>();
+        for (String word: this.words){
+            if (word.endsWith(String.valueOf(c))){
+                outputList.add(word);
+            }
+        }
+        String[] output = outputList.toArray(new String[outputList.size()]);
+        return output;
+    }
 }
